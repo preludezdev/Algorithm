1f2d1e454cbd6be07668f5f09fe0731525337cf0
taehwijeong <preludez.dev@gmail.com>
2021-01-28 08:29:39 +0900

    [20200128]프로그래머스:k번째수

--- src/main/java/programmers/k번째수/Solution.kt
+++ src/main/java/programmers/k번째수/Solution.kt
@@ -6,12 +6,12 @@ class Solution {
         val answer = mutableListOf<Int>()
 
         // 주어진 배열(array)에서 command 영역만큼 자른 뒤 오름차순으로 정렬하고 k번째 숫자를 구해 저장한다.
-        commands.forEach {command ->
+        commands.forEach { command ->
             val startIndex = command[0] - 1
             val endIndex = command[1] - 1
             val k = command[2] - 1
 
-            val numAtK = array.slice(startIndex .. endIndex).sorted()[k]
+            val numAtK = array.slice(startIndex..endIndex).sorted()[k]
             answer.add(numAtK)
         }
 
