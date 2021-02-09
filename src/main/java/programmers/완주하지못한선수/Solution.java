c2ff1ad551f2ee633eeb53175c6bc41f484b7a24
taehwijeong <preludez.dev@gmail.com>
2021-02-09 23:59:40 +0900

    [20210208]프로그래머스:올바른 괄호 - README.md

---
+++ src/main/java/programmers/완주하지못한선수/Solution.java
@@ -0,0 +1,16 @@
+package programmers.완주하지못한선수;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+
+
+
+
+        return answer;
+    }
+}
+
+/**
+ *
+ */
---
+++ src/test/java/programmers/완주하지못한선수/SolutionTest.kt
@@ -0,0 +1,16 @@
+package programmers.완주하지못한선수
+
+import org.junit.Assert
+import org.junit.Test
+
+class SolutionTest {
+
+    @Test
+    fun solutionTest() {
+        val sol = Solution()
+
+        Assert.assertEquals("leo", sol.solution(arrayOf("leo", "kiki", "eden"), arrayOf("leo", "kiki")))
+        Assert.assertEquals("vinko", sol.solution(arrayOf("marina", "josipa", "nikola", "vinko", "filipa"), arrayOf("josipa", "filipa", "marina", "nikola")))
+        Assert.assertEquals("mislav", sol.solution(arrayOf("mislav", "stanko", "mislav", "ana"), arrayOf("stanko", "ana", "mislav")))
+    }
+}
